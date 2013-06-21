fec0ef79a0bb476cfa46760fa5fe8af382ab7213
vellebue <vellebue@gmail.com>
2013-06-21 23:59:37 +0200

    Added explicit empty constructor.

--- src/main/java/org/rulemaker/model/Term.java
+++ src/main/java/org/rulemaker/model/Term.java
@@ -8,6 +8,17 @@ public class Term {
 	private TermType expressionType;
 	private String expressionValue;
 	
+	public Term() {
+	}
+	
+	public Term(String identifier, TermType expressionType,
+			String expressionValue) {
+		super();
+		this.identifier = identifier;
+		this.expressionType = expressionType;
+		this.expressionValue = expressionValue;
+	}
+
 	public String getIdentifier() {
 		return identifier;
 	}
@@ -30,5 +41,36 @@ public class Term {
 	
 	public void setExpressionValue(String expressionValue) {
 		this.expressionValue = expressionValue;
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = (this.identifier != null) ? this.identifier.hashCode() : 0;
+		hashCode = 37*hashCode + ((this.expressionType != null) ? this.expressionType.hashCode() : 0);
+		hashCode = 37*hashCode + ((this.expressionValue != null) ? this.expressionValue.hashCode() : 0);
+		return hashCode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Term) {
+			Term term = (Term) obj;
+			return ((this.identifier != null) ? this.identifier.equals(term.getIdentifier()) : (term.getIdentifier() == null)) &&
+				   ((this.expressionType != null) ? this.expressionType.equals(term.getExpressionType()) : (term.getExpressionType() == null)) &&
+				   ((this.expressionValue != null) ? this.expressionValue.equals(term.getExpressionValue()) : (term.getExpressionValue() == null));
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("Term{");
+		buffer.append("identifier:" + this.identifier + ", ");
+		buffer.append("expressionType:" + this.expressionType + ", ");
+		buffer.append("expressionValue:" + this.expressionValue);
+		buffer.append("}");
+		return buffer.toString();
 	}
 }
