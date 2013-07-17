a48c1ee73fbbf446c31c41be31ed16cffff06f8f
vellebue <vellebue@gmail.com>
2013-07-17 22:21:36 +0200

    Code refactored.

--- src/main/java/org/rulemaker/engine/expressions/OgnlExpressionSolver.java
+++ src/main/java/org/rulemaker/engine/expressions/OgnlExpressionSolver.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import net.sf.cglib.beans.BeanGenerator;
 import net.sf.cglib.core.Predicate;
 import ognl.Ognl;
-import ognl.OgnlContext;
 import ognl.OgnlException;
 
 import org.apache.commons.beanutils.PropertyUtils;
@@ -15,44 +14,49 @@ public class OgnlExpressionSolver implements ExpressionSolver {
 
 	public Object eval(Map<String, Object> contextMap, String expression)
 			throws InvalidExpressionException {
-		//Object parsedExpression = Ognl.parseExpression(expression);
-		OgnlContext ognlContext = new OgnlContext(contextMap);
 		try {
-			return Ognl.getValue(expression, contextMap, buildRootObject(contextMap));
+			return Ognl.getValue(expression, contextMap,
+					buildRootObject(contextMap));
 		} catch (OgnlException e) {
 			throw new InvalidExpressionException(e);
 		}
 	}
-	
+
+	/**
+	 * Builds OGNL root object based on context map, for each
+	 * key-value pair in context map will be a matching property in root
+	 * object with the same value.
+	 * 
+	 * @param contextMap The source context map to build OGNL root object
+	 * @return The resulting root object.
+	 */
 	private Object buildRootObject(Map<String, Object> contextMap) {
 		BeanGenerator bg = new BeanGenerator();
-	    bg.setSuperclass(Object.class);
-	    bg.setNamingPolicy(new net.sf.cglib.core.NamingPolicy() {
-	      public String getClassName(String prefix, String source, Object  /*
-	    			 bg.addProperty("foo", Double.TYPE);
-	    			 g.addProperty("bar", String.class);
-	    			 */key,
-				Predicate names) {
-			return prefix + "Impl";
-	      }
-	    });
-	    for (Map.Entry<String, Object> entry : contextMap.entrySet()) {
-	    	String propertyName = entry.getKey();
-	    	Class<?> propertyType = entry.getValue().getClass();
-	    	bg.addProperty(propertyName, propertyType);
-	    }
-	    Object beanInst = bg.create();
-	    try {
+		bg.setSuperclass(Object.class);
+		bg.setNamingPolicy(new net.sf.cglib.core.NamingPolicy() {
+			public String getClassName(String prefix, String source,
+					Object key, Predicate names) {
+				return prefix + "Impl";
+			}
+		});
+		for (Map.Entry<String, Object> entry : contextMap.entrySet()) {
+			String propertyName = entry.getKey();
+			Class<?> propertyType = entry.getValue().getClass();
+			bg.addProperty(propertyName, propertyType);
+		}
+		Object beanInst = bg.create();
+		try {
 			for (Map.Entry<String, Object> entry : contextMap.entrySet()) {
 				String propertyName = entry.getKey();
 				Object propertyValue = entry.getValue();
-				PropertyUtils.setProperty(beanInst, propertyName, propertyValue); 
+				PropertyUtils
+						.setProperty(beanInst, propertyName, propertyValue);
 			}
 		} catch (Exception e) {
 			// this should never happen
 			throw new RuntimeException(e);
-		} 
-	    return beanInst;
+		}
+		return beanInst;
 	}
 
 }
