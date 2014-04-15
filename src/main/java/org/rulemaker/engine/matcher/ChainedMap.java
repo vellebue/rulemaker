9c4a5caed3ab34ae0a662d6e4b985de33a16d867
vellebue <vellebue@gmail.com>
2014-04-15 21:39:42 +0200

    Added Chained map

---
+++ src/main/java/org/rulemaker/engine/matcher/ChainedMap.java
@@ -0,0 +1,200 @@
+package org.rulemaker.engine.matcher;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
+/**
+ * A chained map is a map built using several maps placed into a 
+ * stack. If you try to add or modify the map you will only interact
+ * with the map in the top of the stack. But if you get a value from 
+ * this chained map you will get it from a deeper map into the stack
+ * if it is not found (null value) in a map near the top. It is
+ * useful to simulate variable scopes in nested environments.
+ * 
+ * @author &Aacute;ngel Garc&iacute;a Bastanchuri
+ *
+ * @param <K> The Type for the key in this chained map.
+ * @param <V> The Type for the value in this chained map.
+ */
+public class ChainedMap<K,V> implements Map<K,V> {
+	
+	private Map<K,V> currentMap;
+	private ChainedMap<K,V> innerChainedMap;
+	
+	/**
+	 * Builds an empty chained map with a single empty map in the top.
+	 */
+	public ChainedMap() {
+		this(new HashMap<K, V>(), null);
+	}
+	
+	/**
+	 * Builds a chained map with a single map from the 
+	 * argument constructor.
+	 * 
+	 * @param map The map used to build this single map.
+	 * 
+	 * @throws NullPointerException If Map argument is null.
+	 * 
+	 */
+	public ChainedMap(Map<K,V> map) throws NullPointerException {
+		this(map, null);
+	}
+	
+	/**
+	 * Builds a chained map chaining the map to the top
+	 * of the given chained map.
+	 * 
+	 * @param map The map that must be put on the top of the given 
+	 *        chained map.
+	 *        
+	 * @param chanedMap The chained map used as a base where the map
+	 *        will be put on the top.
+	 *        
+	 * @throws NullPointerException If Map argument is null.
+	 * 
+	 */
+	public ChainedMap(Map<K,V> map, ChainedMap<K,V> chainedMap) throws NullPointerException {
+		if (map == null) {
+			throw new NullPointerException("Map argument must not be null");
+		} else {
+			currentMap = map;
+			innerChainedMap = chainedMap;
+		}
+	}
+	
+	
+	public int size() {
+		return this.keySet().size();
+	}
+
+	public boolean isEmpty() {
+		if (innerChainedMap == null) {
+			return currentMap.isEmpty();
+		} else {
+			return currentMap.isEmpty() && innerChainedMap.isEmpty();
+		}
+	}
+
+	public boolean containsKey(Object key) {
+		return currentMap.containsKey(key) || 
+			   ((innerChainedMap != null) && innerChainedMap.containsKey(key));
+	}
+
+	public boolean containsValue(Object value) {
+		return whichKeysMapToAValue(value).size() > 0;
+	}
+
+	public V get(Object key) {
+		V value = currentMap.get(key);
+		if ((value == null) && (innerChainedMap != null)) {
+			return innerChainedMap.get(key);
+		} else {
+			return value;
+		}
+	}
+
+	public V put(K key, V value) {
+		V previousValue = get(key);
+		currentMap.put(key, value);
+		return previousValue;
+	}
+
+	public V remove(Object key) {
+		V previousValue = get(key);
+		currentMap.remove(key);
+		if (innerChainedMap != null) {
+			innerChainedMap.remove(key);
+		}
+		return previousValue;
+	}
+
+	public void putAll(Map<? extends K, ? extends V> m) {
+		currentMap.putAll(m);
+	}
+
+	public void clear() {
+		currentMap = new HashMap<K, V>();
+		if (innerChainedMap != null) {
+			innerChainedMap.clear();
+		}
+	}
+
+	public Set<K> keySet() {
+		Set<K> topKeySet = currentMap.keySet();
+		if (innerChainedMap != null) {
+			Set<K> resultSet = new HashSet<K>(topKeySet);
+			resultSet.addAll(innerChainedMap.keySet());
+			return resultSet;
+		} else {
+			return topKeySet;
+		}
+	}
+
+	public Collection<V> values() {
+		Collection<V> values = new HashSet<V>();
+		Set<java.util.Map.Entry<K, V>> entrySet = entrySet();
+		for (java.util.Map.Entry<K, V> anEntry : entrySet) {
+			values.add(anEntry.getValue());
+		}
+		return values;
+	}
+
+	public Set<java.util.Map.Entry<K, V>> entrySet() {
+		Set<java.util.Map.Entry<K, V>> entrySet = currentMap.entrySet();
+		Set<java.util.Map.Entry<K, V>> resultingEntrySet = new HashSet<Map.Entry<K,V>>();
+		resultingEntrySet.addAll(entrySet);
+		if (innerChainedMap != null) {
+			Set<java.util.Map.Entry<K, V>> innerMapEntrySet = 
+					innerChainedMap.entrySet();
+			for (java.util.Map.Entry<K, V> anEntry : innerMapEntrySet) {
+				if (!currentMap.containsKey(anEntry.getKey())) {
+					resultingEntrySet.add(anEntry);
+				}
+			}
+		}
+		return resultingEntrySet;
+	}
+	
+	/**
+	 * Gets the set of keys that map to a certain value
+	 * in this chained map.
+	 * 
+	 * @param value the value whose mapping keys are being looked for.
+	 * 
+	 * @return A set containing those keys that map to the given value.
+	 * 
+	 */
+	private Set<K> whichKeysMapToAValue(Object value) {
+		Set<K> resultingKeySet = new HashSet<K>();
+		if (innerChainedMap == null) {
+			for (K aKey : currentMap.keySet()) {
+				if (currentMap.get(aKey).equals(value)) {
+					resultingKeySet.add(aKey);
+				}
+			}
+		} else {
+			Set<K> topMappingKeys = new ChainedMap<K,V>(currentMap).whichKeysMapToAValue(value);
+			Set<K> innerMappingKeys = innerChainedMap.whichKeysMapToAValue(value);
+			// top map mapping keys are part of the key set solution
+			resultingKeySet.addAll(topMappingKeys);
+			// inner mapping keys are part of the solution
+			// unless they are "buried" by a different value mapped to the same key
+			for (K aKey : innerMappingKeys) {
+				if (currentMap.containsKey(aKey)) {
+					if (currentMap.get(aKey).equals(value)) {
+						resultingKeySet.add(aKey);
+					}
+				} else {
+					resultingKeySet.add(aKey);
+				}
+			}
+		}
+		return resultingKeySet;
+	}
+
+}
---
+++ src/test/java/org/rulemaker/engine/matcher/ChainedMapTest.java
@@ -0,0 +1,281 @@
+package org.rulemaker.engine.matcher;
+
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChainedMapTest {
+	
+	private Map<String, String> mapZero;
+	private Map<String, String> mapOne;
+	
+	@Before
+	public void initTestMaps() {
+		mapZero = new HashMap<String, String>();
+		mapZero.put("X", "valueX");
+		mapZero.put("Y", "valueY");
+		mapOne = new HashMap<String, String>();
+		mapOne.put("X", "valueXNew");
+		mapOne.put("Z", "valueZ");
+	}
+	
+	@Test
+	public void shouldGetSizeZeroForAnEmptyChainedMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>();
+		assertEquals(0, chainedMap.size());
+	}
+	
+	@Test
+	public void shouldGetSizeTwoForAChainedMapBasedOnMapZero() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapZero);
+		assertEquals(2, chainedMap.size());
+	}
+	
+	@Test
+	public void shouldGetSizeThreeForAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertEquals(3, chainedMap.size());
+	}
+	
+	@Test
+	public void shouldIsEmptyReturnTrueForAnEmptyChainedMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>();
+		assertTrue(chainedMap.isEmpty());
+	}
+	
+	@Test
+	public void shouldIsEmptyReturnFalseForAChainedMapBasedOnMapZero() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapZero);
+		assertFalse(chainedMap.isEmpty());
+	}
+	
+	@Test
+	public void shouldIsEmptyReturnFalseForAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertFalse(chainedMap.isEmpty());
+	}
+	
+	@Test
+	public void shouldRecognizeYAsAContainedKeyForAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertTrue(chainedMap.containsKey("Y"));
+	}
+	
+	@Test
+	public void shouldRecognizeXAsAContainedKeyForAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertTrue(chainedMap.containsKey("Z"));
+	}
+	
+	@Test
+	public void shouldRecognizeWIsNotAContainedKeyForAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertFalse(chainedMap.containsKey("W"));
+	}
+	
+	@Test
+	public void shouldContainValueZInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertTrue(chainedMap.containsValue("valueZ"));
+	}
+	
+	@Test
+	public void shouldContainValueXNewInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertTrue(chainedMap.containsValue("valueXNew"));
+	}
+	
+	@Test
+	public void shouldNotContainValueWInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertFalse(chainedMap.containsValue("valueW"));
+	}
+	
+	@Test
+	public void shouldNotContainValueXInAChainedMapBasedOnMapZeroAndMapOneDueValueXHasBeenOverwrittenByMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertFalse(chainedMap.containsValue("valueW"));
+	}
+	
+	@Test
+	public void shouldGetXReturnValueXNewInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertEquals("valueXNew", chainedMap.get("X"));
+	}
+	
+	@Test
+	public void shouldGetYReturnValueYInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertEquals("valueY", chainedMap.get("Y"));
+	}
+	
+	@Test
+	public void shouldGetZReturnValueZInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		assertEquals("valueZ", chainedMap.get("Z"));
+	}
+	
+	@Test
+	public void shouldPutWWithValueValueWInAChainedMapBasedOnMapZeroAndMapOneCorrectly() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		String previousValue = chainedMap.put("W", "valueW");
+		assertEquals("valueW", chainedMap.get("W"));
+		assertNull(previousValue);
+	}
+	
+	@Test
+	public void shouldOverrideZKeyWithValueValueZNewInAChainedMapBasedOnMapZeroAndMapOneCorrectly() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		String previousValue = chainedMap.put("Z", "valueZNew");
+		assertEquals("valueZNew", chainedMap.get("Z"));
+		assertEquals("valueZ", previousValue);
+	}
+	
+	@Test
+	public void shouldRemoveANonExistingKeyReturningNullInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		String previousValue = chainedMap.remove("W");
+		assertNull(chainedMap.get("W"));
+		assertNull(previousValue);
+	}
+	
+	@Test
+	public void shouldRemoveAnOverwrittenKeyEntirelyInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		String previousValue = chainedMap.remove("X");
+		assertNull(chainedMap.get("X"));
+		assertEquals("valueXNew", previousValue);
+	}
+	
+	@Test
+	public void shouldRemoveAnInnerKeyCorrectlyInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		String previousValue = chainedMap.remove("Z");
+		assertNull(chainedMap.get("Z"));
+		assertEquals("valueZ", previousValue);
+	}
+	
+	@Test
+	public void shouldPerformPutAllCorrectlyInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		Map<String, String> mapTwo = new HashMap<String, String>();
+		mapTwo.put("W", "valueW");
+		mapTwo.put("Z", "valueZNew");
+		chainedMap.putAll(mapTwo);
+		assertEquals("valueW", chainedMap.get("W"));
+		assertEquals("valueZNew", chainedMap.get("Z"));
+	}
+	
+	@Test
+	public void shouldPerformClearCorrectlyInAChainedMapBasedOnMapZeroAndMapOne() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		chainedMap.clear();
+		assertNull(chainedMap.get("X"));
+		assertNull(chainedMap.get("Y"));
+		assertNull(chainedMap.get("Z"));
+	}
+	
+	@Test
+	public void shouldObtainAnEmptyKeySetForEmptyChainedMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>();
+		assertEquals(new HashSet<String>(), chainedMap.keySet());
+	}
+	
+	@Test
+	public void shouldObtainTheSameKeySetAsMapZeroForAChainedMapBasedOnMapZero() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapZero);
+		assertEquals(mapZero.keySet(), chainedMap.keySet());
+	}
+	
+	@Test
+	public void shouldObtainTheGivenKeySetForChainedMapBasedOnMapZeroAndMapOne() {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		Set<String> expectedResult = new HashSet<String>(Arrays.asList(new String []{"X", "Y", "Z"}));
+		assertEquals(expectedResult, chainedMap.keySet());
+	}
+	
+	@Test
+	public void shouldGetAnEmptyValueSetForAnEmptyChainedMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>();
+		Set<String> actualValues = new HashSet<String>();
+		actualValues.addAll(chainedMap.values());
+		assertEquals(new HashSet<String>(), actualValues);
+	}
+	
+	@Test
+	public void shouldGetTheSameValueCollectionForMapZeroValueSetAndChainedMapBasedOnZeroMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapZero);
+		Set<String> expectedValues = new HashSet<String>();
+		expectedValues.addAll(mapZero.values());
+		Set<String> actualValues = new HashSet<String>();
+		actualValues.addAll(chainedMap.values());
+		assertEquals(expectedValues, actualValues);
+	}
+	
+	@Test
+	public void shouldGetAValueCollectionContainingTheRightValuesForChainedMapBasedOnMapZeroAndMapOne() {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		Set<String> expectedValues = new HashSet<String>();
+		expectedValues.addAll(Arrays.asList(new String[]{"valueXNew", "valueY", "valueZ"}));
+		Set<String> actualValues = new HashSet<String>();
+		actualValues.addAll(chainedMap.values());
+		assertEquals(expectedValues, actualValues);
+	}
+	
+	@Test
+	public void shouldGetAnEmptyEntrySetForAnEmptyChainedMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>();
+		Set<java.util.Map.Entry<String, String>> actualValues = new HashSet<java.util.Map.Entry<String, String>>();
+		actualValues.addAll(chainedMap.entrySet());
+		assertEquals(new HashSet<java.util.Map.Entry<String, String>>(), actualValues);
+	}
+	
+	@Test
+	public void shouldGetTheEntrySetForMapZeroValueSetAndChainedMapBasedOnZeroMap() throws Exception {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapZero);
+		assertEquals(mapZero.entrySet(), chainedMap.entrySet());
+	}
+	
+	@Test
+	public void shouldGetAnEntrySetContainingTheRightValuesForChainedMapBasedOnMapZeroAndMapOne() {
+		ChainedMap<String, String> chainedMap = new ChainedMap<String, String>(mapOne, 
+				new ChainedMap<String, String>(mapZero));
+		Set<java.util.Map.Entry<String, String>> expectedValues = new HashSet<java.util.Map.Entry<String, String>>();
+		expectedValues.add(new AbstractMap.SimpleEntry<String, String>("X", "valueXNew"));
+		expectedValues.add(new AbstractMap.SimpleEntry<String, String>("Y", "valueY"));
+		expectedValues.add(new AbstractMap.SimpleEntry<String, String>("Z", "valueZ"));
+		Set<java.util.Map.Entry<String, String>> actualValues = new HashSet<java.util.Map.Entry<String, String>>();
+		actualValues.addAll(chainedMap.entrySet());
+		assertEquals(expectedValues, actualValues);
+	}
+
+}
